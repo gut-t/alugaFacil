bedb3bad67b496ecdd990ec05eb362037c44c53d
gut-t <estudantegut@gmail.com>
2024-09-04 17:35:36 -0300

    Add files via upload

---
+++ alugaFacil_v1/model/DAO/UsuarioDAO.php
@@ -0,0 +1,18 @@
+<?php
+class UsuarioDAO{
+ public function cadastrarUsuario(){}
+ public function listarUsuarios(){}
+ public function excluirUsuarios(){}
+ public function alterarUsuario(){}
+ public function buscarUsuario(){}
+ public function login(){}
+
+
+}
+
+
+
+
+
+
+?>
