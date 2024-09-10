fca85ce90a5b5c3125e8350b31f19198afe25420
gut-t <estudantegut@gmail.com>
2024-09-10 15:43:18 -0300

    Add files via upload

---
+++ alugaFacil_v2_conexao/model/DAO/testeConexao.php
@@ -0,0 +1,18 @@
+<?php
+try {
+    //tentativa de conexão
+    $options = array(
+        PDO::ATTR_PERSISTENT => true,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    $conexao = new PDO(dsn: "mysql:host=localhost; dbname=testeconexao", username: "root", password: "", options: $options);
+    echo "Conexão realizada com sucesso";
+
+} catch (PDOException $exe) {
+    //tratar o erro caso a conexao falhe
+    echo "Deu erro, mostre o erro: ";
+    echo "O erro é " . $exe->getMessage();
+}
+?>
